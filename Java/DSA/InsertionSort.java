696c731d3a64545eea0e472870d79da587a222f5
Ananya Maheshwari <92998900+pinkelephant4@users.noreply.github.com>
2023-10-15 20:40:02 +0530

    Add files via upload

---
+++ Java/DSA/InsertionSort.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+
+public class InsertionSort {
+    public static void main(String args[])
+    {
+        int arr[] = { 12, 11, 13, 5, 6 };
+
+        InsertionSort ob = new InsertionSort();
+        ob.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+
+    void sort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+
+}
+
+
+
